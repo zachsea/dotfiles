8d3cec82c9e5c6a7a6243e61801ed257bfdf0237
zachsea <zachse4@gmail.com>
2026-01-23 23:42:41 -0800

    nvim: 6502 syntax highlights

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,16 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "TSUpdate",
+  callback = function()
+    require("nvim-treesitter.parsers").ca65 = {
+      install_info = {
+        url = "https://github.com/zachsea/tree-sitter-ca65",
+        queries = "queries/ca65",
+      },
+    }
+  end,
+})
+
+vim.treesitter.language.register("ca65", { "asm" })
